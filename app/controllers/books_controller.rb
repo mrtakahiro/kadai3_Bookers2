dc39d7130c550787c15a19529729bf22e56bbc39
mrtakahiro <128272534+mrtakahiro@users.noreply.github.com>
2023-05-14 12:50:15 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,28 @@
 class BooksController < ApplicationController
   def new
-    @book_image = Books.new
+    @book = Book.new
+  end
+
+  def create
+    @book = PostImage.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to post_images_path
   end
 
   def index
-    @book_images = Books.page(params[:page])
+    @book = Book.all
   end
 
   def show
-    @book_image = Books.find(params[:id])
-    @book_comment = Books.new
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @book_images = @user.post_images.page(params[:page])
+    @book = @user.post_images.page(params[:page])
   end
 
   def edit
