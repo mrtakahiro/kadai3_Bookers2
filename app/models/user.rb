753f9873a81417b20aada699290ed5bc07adfe35
mrtakahiro <128272534+mrtakahiro@users.noreply.github.com>
2023-05-16 12:48:55 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      image.attach(io: File.open(file_path), filename: 'no-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [100, 100]).processed
   end
 end
